1708c18a7e52c1b28d98f03d461c156e205877ca
Hanna Eriksson <erikssonh659@gmail.com>
2026-09-13 10:55:33 +0200

    Create users database table

--- database.js
+++ database.js
@@ -5,6 +5,22 @@ const db = new sqlite3.Database("./timetotalk.db", (err) => {
         console.error("Kunde inte öppna databasen:", err.message);
     } else {
         console.log("SQLite-databasen är ansluten.");
+
+        db.run(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT UNIQUE NOT NULL,
+                email TEXT UNIQUE NOT NULL UNIQUE,
+                password TEXT NOT NULL,
+                role TEXT NOT NULL CHECK(role IN ('student', 'teacher'))
+            )
+        `, (err) => {
+            if (err) {
+                console.error("Kunde inte skapa tabellen:", err.message);
+            } else {
+                console.log("Tabellen 'users' är skapad eller finns redan.");
+            }
+        });
     }
 });
 
