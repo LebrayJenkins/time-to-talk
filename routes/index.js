d24d727ec440eb754d1e7f517e797e7e0c28fb6c
Adrian Leidstedt <adrianleidstedt@gmail.com>
2026-09-15 22:22:57 +0200

    Create student dashboard with reusable student sidebar and responsive CSS

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db= require("../database");
+const db = require("../database");
 
 const router = express.Router();
 
@@ -10,53 +10,60 @@ router.get("/", (req, res) => {
 });
 
 router.get("/teacher/tider/skapa", (req, res) => {
-    res.render("teacher-create-time");
+  res.render("teacher-create-time");
 });
 
 router.post("/teacher/tider/skapa", (req, res) => {
-    const activity = req.body.activity;
-    const date = req.body.date;
-    const startTime = req.body.start_time;
-    const endTime = req.body.end_time;
+  const activity = req.body.activity;
+  const date = req.body.date;
+  const startTime = req.body.start_time;
+  const endTime = req.body.end_time;
 
-    console.log("Vald aktivitet:", activity);
-    console.log("Datum:", date);
-    console.log("Starttid:", startTime);
-    console.log("Sluttid:", endTime);
+  console.log("Vald aktivitet:", activity);
+  console.log("Datum:", date);
+  console.log("Starttid:", startTime);
+  console.log("Sluttid:", endTime);
 
-    const today = new Date().toISOString().split("T")[0];
+  const today = new Date().toISOString().split("T")[0];
 
-    if (date < today) {
-        return res.status(400).send("Datumet kan inte vara tidigare än dagens datum.");
-    }
-    if (endTime <= startTime) {
-        return res.status(400).send("Sluttiden måste vara efter starttiden.");
-    }
+  if (date < today) {
+    return res
+      .status(400)
+      .send("Datumet kan inte vara tidigare än dagens datum.");
+  }
+  if (endTime <= startTime) {
+    return res.status(400).send("Sluttiden måste vara efter starttiden.");
+  }
 
-    const teacherId = 1; // Hårdkodad lärar-ID för teständamål
-   
-     db.createAvailableTime(
-        teacherId,
-        date,
-        startTime,
-        endTime,
-        activity,
-        (err, time) => {
-            if (err) {
-                console.error("Fel när tiden skulle sparas:", err.message);
-                return res.status(500).send("Kunde inte spara tiden.");
-            }
+  const teacherId = 1; // Hårdkodad lärar-ID för teständamål
 
-            console.log("Tid sparad:", time);
+  db.createAvailableTime(
+    teacherId,
+    date,
+    startTime,
+    endTime,
+    activity,
+    (err, time) => {
+      if (err) {
+        console.error("Fel när tiden skulle sparas:", err.message);
+        return res.status(500).send("Kunde inte spara tiden.");
+      }
 
-            res.send(`
+      console.log("Tid sparad:", time);
+
+      res.send(`
               <h1>Tiden är skapad!</h1>
               <p>Aktivitet: ${time.activity}</p>
               <p>Datum: ${time.date}</p>
               <p>Tid: ${time.startTime} - ${time.endTime}</p>
             `);
-        }
-      );
+    },
+  );
 });
 
+router.get("/student-dashboard", (req, res) => {
+  res.render("student-dashboard", {
+    title: "Min översikt",
+  });
+});
 module.exports = router;
